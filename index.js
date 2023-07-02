aced7102b85e24ceb9abbd04080146dab3a62219
typhoon890 <designamoop19@gmail.com>
2023-07-02 11:21:55 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImage="dice"+ randomNumber1 +".png";
+var randomImage ="images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImage);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceImage2="dice"+randomNumber2+".png";
+var randomImage2 ="images/"+randomDiceImage2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImage2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+
+}
+else if(randomNumber1<randomNumbe2)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else
+{
+    document.querySelector("h1").innerHTML="DRAW!";
+}
